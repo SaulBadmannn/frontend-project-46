360f9eb7812a848a00fe5269d35aba80b5ce6000
SaulBadmannn <samat7089@gmail.com>
2024-04-01 17:57:40 +0500

    make step 3

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,54 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import _ from 'lodash';
+
+import parseJsonToObj from './parser.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const dataFile1 = parseJsonToObj(filepath1);
+    const dataFile2 = parseJsonToObj(filepath2);
+
+    const keysFile1 = Object.keys(dataFile1);
+    const keysFile2 = Object.keys(dataFile2);
+
+    const allKeys = _.union(keysFile1, keysFile2);
+    const sortUniqKeys = _.uniq(allKeys).sort();
+
+    const result = sortUniqKeys.reduce((acc, currentKey) => {
+      let newAcc = acc;
+      if (Object.hasOwn(dataFile1, currentKey) && Object.hasOwn(dataFile2, currentKey)) {
+        if (dataFile1[currentKey] === dataFile2[currentKey]) {
+          newAcc += `  ${currentKey}: ${dataFile1[currentKey]}\n`;
+
+          return newAcc;
+        }
+        newAcc += `- ${currentKey}: ${dataFile1[currentKey]}\n`;
+        newAcc += `+ ${currentKey}: ${dataFile2[currentKey]}\n`;
+
+        return newAcc;
+      }
+
+      if (Object.hasOwn(dataFile2, currentKey)) {
+        newAcc += `+ ${currentKey}: ${dataFile2[currentKey]}\n`;
+
+        return newAcc;
+      }
+
+      newAcc += `- ${currentKey}: ${dataFile1[currentKey]}\n`;
+
+      return newAcc;
+    }, '');
+
+    console.log(`{\n${result}}`);
+  });
 
 program.parse();
---
+++ parser.js
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path';
+import { cwd } from 'node:process';
+import { readFileSync } from 'node:fs';
+
+const getAbsoluteFilePath = (filePath) => {
+  const fileName = filePath.split('/').at(-1);
+
+  return resolve(cwd(), fileName);
+};
+
+const parseFileToObj = (filePath) => {
+  const absoluteFilePath = getAbsoluteFilePath(filePath);
+  const fileExtension = filePath.split('/').at(-1).split('.').at(-1);
+
+  if (fileExtension === 'json') {
+    return JSON.parse(readFileSync(absoluteFilePath));
+  }
+};
+
+export default parseFileToObj;
