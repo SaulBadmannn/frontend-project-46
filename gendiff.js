2ec980c9ccf2837a2f5515f75b2f8f266cccc5cf
SaulBadmannn <samat7089@gmail.com>
2024-06-03 13:48:17 +0500

    moved genDiff to index.js

--- gendiff.js
+++ gendiff.js
@@ -1,47 +1,10 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import _ from 'lodash';
-
+import genDiff from './src/index.js';
 import parseFileToObj from './parser.js';
 
 const program = new Command();
-
-const genDiff = (dataFile1, dataFile2) => {
-  const keysFile1 = Object.keys(dataFile1);
-  const keysFile2 = Object.keys(dataFile2);
-
-  const allKeys = _.union(keysFile1, keysFile2);
-  const sortUniqKeys = _.uniq(allKeys).sort();
-
-  const result = sortUniqKeys.reduce((acc, currentKey) => {
-    let newAcc = acc;
-
-    if (Object.hasOwn(dataFile1, currentKey) && Object.hasOwn(dataFile2, currentKey)) {
-      if (dataFile1[currentKey] === dataFile2[currentKey]) {
-        newAcc += `  ${currentKey}: ${dataFile1[currentKey]}\n`;
-
-        return newAcc;
-      }
-      newAcc += `- ${currentKey}: ${dataFile1[currentKey]}\n`;
-      newAcc += `+ ${currentKey}: ${dataFile2[currentKey]}\n`;
-
-      return newAcc;
-    }
-
-    if (Object.hasOwn(dataFile2, currentKey)) {
-      newAcc += `+ ${currentKey}: ${dataFile2[currentKey]}\n`;
-
-      return newAcc;
-    }
-
-    newAcc += `- ${currentKey}: ${dataFile1[currentKey]}\n`;
-
-    return newAcc;
-  }, '');
-
-  return `{\n${result}}`;
-};
 
 program
   .description('Compares two configuration files and shows a difference.')
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import _ from 'lodash';
+
+const genDiff = (dataFile1, dataFile2) => {
+  const keysFile1 = Object.keys(dataFile1);
+  const keysFile2 = Object.keys(dataFile2);
+
+  const allKeys = _.union(keysFile1, keysFile2);
+  const sortUniqKeys = _.uniq(allKeys).sort();
+
+  const result = sortUniqKeys.reduce((acc, currentKey) => {
+    let newAcc = acc;
+
+    if (Object.hasOwn(dataFile1, currentKey) && Object.hasOwn(dataFile2, currentKey)) {
+      if (dataFile1[currentKey] === dataFile2[currentKey]) {
+        newAcc += `  ${currentKey}: ${dataFile1[currentKey]}\n`;
+
+        return newAcc;
+      }
+      newAcc += `- ${currentKey}: ${dataFile1[currentKey]}\n`;
+      newAcc += `+ ${currentKey}: ${dataFile2[currentKey]}\n`;
+
+      return newAcc;
+    }
+
+    if (Object.hasOwn(dataFile2, currentKey)) {
+      newAcc += `+ ${currentKey}: ${dataFile2[currentKey]}\n`;
+
+      return newAcc;
+    }
+
+    newAcc += `- ${currentKey}: ${dataFile1[currentKey]}\n`;
+
+    return newAcc;
+  }, '');
+
+  return `{\n${result}}`;
+};
+
+export default genDiff;
